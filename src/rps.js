c4bf30c1feca7c139e4c12a8d4546f32eddc39e0
trayan-iliev <trayan.iliev@gmail.com>
2023-08-22 11:27:33 +0300

    Finished design & functionality

--- src/rps.js
+++ src/rps.js
@@ -17,16 +17,57 @@ function playRpsRound (playerSelection) {
 
     let doYouWin = '';
 
-    if (computerChoice == playerChoice) {doYouWin = "It's a draw"} else {
-        if (playerChoice == 'rock' & computerChoice == 'paper') { doYouWin = "You lose"}
-        else if (playerChoice == 'rock' & computerChoice == 'scissors') { doYouWin = "You win"}
-        else if (playerChoice == 'paper' & computerChoice == 'rock') { doYouWin = "You win"}
-        else if (playerChoice == 'paper' & computerChoice == 'scissors') { doYouWin = "You lose"}
-        else if (playerChoice == 'scissors' & computerChoice == 'rock') { doYouWin = "You lose"}
-        else if (playerChoice == 'scissors' & computerChoice == 'paper') { doYouWin = "You win"}
+    if (computerChoice == playerChoice) {doYouWin = "It's a draw!"} else {
+        if (playerChoice == 'rock' & computerChoice == 'paper') { doYouWin = "You lose!"}
+        else if (playerChoice == 'rock' & computerChoice == 'scissors') { doYouWin = "You win!"}
+        else if (playerChoice == 'paper' & computerChoice == 'rock') { doYouWin = "You win!"}
+        else if (playerChoice == 'paper' & computerChoice == 'scissors') { doYouWin = "You lose!"}
+        else if (playerChoice == 'scissors' & computerChoice == 'rock') { doYouWin = "You lose!"}
+        else if (playerChoice == 'scissors' & computerChoice == 'paper') { doYouWin = "You win!"}
     }
+   
+    revealResult (playerChoice, computerChoice, doYouWin)
+    // alert (`You selected ${playerChoice}, RPS Bot selected ${computerChoice}. \n${doYouWin}!`);
+}
 
-
+function revealResult (playerChoice, computerChoice, doYouWin) {
     
-    alert (`You selected ${playerChoice}, RPS Bot selected ${computerChoice}. \n${doYouWin}!`);
+    // Select containers
+    let textEle = document.getElementById('results-text');
+    let imageEle = document.getElementById('results-image');
+    let resultEle = document.getElementById('results-result');
+    
+    // Clear containers from previous messages
+    while (textEle.firstChild) {
+        textEle.classList.remove('fade-in');
+        textEle.removeChild(textEle.firstChild);
+    }
+    while (imageEle.firstChild) {
+        imageEle.classList.remove('fade-in');
+        imageEle.removeChild(imageEle.firstChild);
+    }
+    while (resultEle.firstChild) {
+        resultEle.classList.remove('fade-in');
+        resultEle.removeChild(resultEle.firstChild);
+    }
+    
+    
+    // Create content
+    let resultText = document.createTextNode (`Opponent:`);
+    let resultImage = document.createElement('img');
+    let resultResult = document.createTextNode(doYouWin);
+    if (computerChoice == 'rock') {resultImage.src = '../img/rock-color.png'}
+    else if (computerChoice == 'paper') {resultImage.src = '../img/paper-color.png'}
+    else if (computerChoice == 'scissors') {resultImage.src = '../img/scissors-color.png'}
+
+    // Append content
+    textEle.appendChild(resultText);
+    imageEle.appendChild(resultImage);
+    resultEle.appendChild(resultResult);
+
+    // Apply fade-in class
+    textEle.classList.add('fade-in');
+    imageEle.classList.add('fade-in');
+    resultEle.classList.add('fade-in');
+
 }
