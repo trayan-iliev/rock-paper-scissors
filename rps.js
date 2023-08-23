393b28417e2941bd3f4ecab8d9cb3d33dab45f2f
trayan-iliev <trayan.iliev@gmail.com>
2023-08-23 11:55:59 +0300

    Update - Updated design - Fixed / updated fade-in/fade-out elements - Added Play again? - Froze game choice until Play Again is pressed. -Added a placeholder for score keeping

--- rps.js
+++ rps.js
@@ -33,42 +33,84 @@ function playRpsRound (playerSelection) {
 function revealResult (playerChoice, computerChoice, doYouWin) {
     
     // Select containers
-    let textEle = document.getElementById('results-text');
+    let resultEle = document.getElementById('results-text');
     let imageEle = document.getElementById('results-image');
-    let resultEle = document.getElementById('results-result');
+    let playagainEle = document.getElementById('results-playagain');
     
-    // Clear containers from previous messages
-    while (textEle.firstChild) {
-        textEle.classList.remove('fade-in');
-        textEle.classList.add('fade-out');
-        textEle.removeChild(textEle.firstChild);
-    }
-    while (imageEle.firstChild) {
-        imageEle.classList.remove('fade-in');
-        imageEle.removeChild(imageEle.firstChild);
-    }
-    while (resultEle.firstChild) {
-        resultEle.classList.remove('fade-in');
-        resultEle.removeChild(resultEle.firstChild);
-    }
-    
-    
+    // Permanently change the color of the selected option
+    if (playerChoice == 'rock') {document.getElementById('rock-image').src = 'img/rock-color.png'}
+    else if (playerChoice == 'paper') {document.getElementById('paper-image').src = 'img/paper-color.png'}
+    else if (playerChoice == 'scissors') {document.getElementById('scissors-image').src = 'img/scissors-color.png'};
+
+    // Disable hyperlinks after a choice is made
+    document.getElementById('rock-link').removeAttribute("onclick");
+    document.getElementById('rock-link').removeAttribute("href");
+    document.getElementById('paper-link').removeAttribute("onclick");
+    document.getElementById('paper-link').removeAttribute("href");
+    document.getElementById('scissors-link').removeAttribute("onclick");
+    document.getElementById('scissors-link').removeAttribute("href");
+    document.getElementById('rock-image-hover').classList.remove('game-choice-color-image');
+    document.getElementById('paper-image-hover').classList.remove('game-choice-color-image');
+    document.getElementById('scissors-image-hover').classList.remove('game-choice-color-image');
+
     // Create content
-    let resultText = document.createTextNode (`Opponent:`);
     let resultImage = document.createElement('img');
     let resultResult = document.createTextNode(doYouWin);
+    let resultPlayAgain = document.createTextNode (`Play again?`);
     if (computerChoice == 'rock') {resultImage.src = 'img/rock-color.png'}
     else if (computerChoice == 'paper') {resultImage.src = 'img/paper-color.png'}
     else if (computerChoice == 'scissors') {resultImage.src = 'img/scissors-color.png'}
 
     // Append content
-    textEle.appendChild(resultText);
     imageEle.appendChild(resultImage);
     resultEle.appendChild(resultResult);
+    playagainEle.appendChild(resultPlayAgain);
 
     // Apply fade-in class
-    textEle.classList.add('fade-in');
     imageEle.classList.add('fade-in');
     resultEle.classList.add('fade-in');
+    playagainEle.classList.add('fade-in');
+}
+
+function playAgain() {
+    // Select containers
+    let resultEle = document.getElementById('results-text');
+    let imageEle = document.getElementById('results-image');
+    let playagainEle = document.getElementById('results-playagain');
+
+    // Return selection image color back to grayscale
+    document.getElementById('rock-image').src = 'img/rock-bw.png';
+    document.getElementById('paper-image').src = 'img/paper-bw.png';
+    document.getElementById('scissors-image').src = 'img/scissors-bw.png';
+
+    // Clear containers from previous messages
+    resultEle.classList.add('fade-out');
+    imageEle.classList.add('fade-out');
+    playagainEle.classList.add('fade-out');
+
+    setTimeout(() => {
+    while (resultEle.firstChild) {
+        resultEle.removeChild(resultEle.firstChild);
+        resultEle.classList.remove('fade-out');
+    }
+    while (imageEle.firstChild) {
+        imageEle.removeChild(imageEle.firstChild);
+        imageEle.classList.remove('fade-out');
+    }
+    while (playagainEle.firstChild) {
+        playagainEle.removeChild(playagainEle.firstChild);
+        playagainEle.classList.remove('fade-out');
+    } }, 400);
+
+    // Reactivate hyperlink & onClick function call
+    document.getElementById('rock-link').setAttribute('href', '#');
+    document.getElementById('rock-link').setAttribute('onclick', 'playRpsRound(0)');
+    document.getElementById('paper-link').setAttribute('href', '#');
+    document.getElementById('paper-link').setAttribute('onclick', 'playRpsRound(1)');
+    document.getElementById('scissors-link').setAttribute('href', '#');
+    document.getElementById('scissors-link').setAttribute('onclick', 'playRpsRound(2)');
+    document.getElementById('rock-image-hover').classList.add('game-choice-color-image');
+    document.getElementById('paper-image-hover').classList.add('game-choice-color-image');
+    document.getElementById('scissors-image-hover').classList.add('game-choice-color-image');
 
 }
